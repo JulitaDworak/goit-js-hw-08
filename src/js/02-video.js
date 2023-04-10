0986f754897eaaa15a45492a9fa8d0cfb6b00d2d
JulitaDworak <julita.dworak@gmail.com>
2023-04-10 08:15:44 +0200

    hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,40 +1,22 @@
 
 import Vimeo from "@vimeo/player"
 import "@vimeo/player/dist/player"
-
-const LOCALSTORAGE_VIDEO = 'videoplayer-current-time'
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
+const LOCALSTORAGE_VIDEO = 'videoplayer-current-time'
+
+const saveVideoTime= throttle(
+    data => localStorage.setItem(LOCALSTORAGE_VIDEO, data.seconds),
+    1000
+  );
+
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then();
+  player.on('timeupdate', saveVideoTime);
 
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
 
-player.on('play', onPlay);
 
-player.on('timeupdate', function(data) {
-    duration: 61.857
-    percent: 0.049
-    seconds: 3.034
-    // data is an object containing properties specific to that event
-}
-);
-
-// const currentVideoTime = player.setCurrenrTime(currentTime)
-// console.log(currentVideoTime);
-
-const playertime = localStorage.setItem("time", data.seconds)
-
-console.log(playertime);
-// localStorage.setItem("videoplayer-current-time",JSON.stringify(videoplayer-current-time) )
-
-// const videoCurrentTime = localStorage.getItem("videoplayer-current-time")
-
-// const time ={
